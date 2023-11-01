70fd903560f8757c9562dfade9a7fa00ec6d82ed
ngecu <devngecu@gmail.com>
2023-11-01 16:53:27 +0300

    frontend introduction with their functionality

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,114 @@
+import { Request, Response } from 'express'
+import mssql from 'mssql'
+import {v4} from 'uuid'
+import bcrypt from 'bcrypt'
+import { sqlConfig } from '../config/sqlConfig'
+import jwt from 'jsonwebtoken'
+// import dotenv from 'dotenv'
+import { LoginUser } from '../interfaces/user'
+import { ExtendedUser } from '../middleware/verifyToken'
+import Connection from '../dbhelpers/dbhelpers'
+
+const dbhelper = new Connection
+
+export const registerUser= async(req:Request, res: Response) =>{
+    try {
+        let {first_name, last_name,password} = req.body
+
+        let user_id = v4()
+
+        const hashedPwd = await bcrypt.hash(password, 5)
+
+
+        
+        let result = dbhelper.execute('registerUser', {
+            user_id, name, first_name, last_name, password: hashedPwd
+        })
+        
+
+        return res.status(200).json({
+            message: 'User  registered successfully'
+        })
+        
+    } catch (error) {
+        return res.json({
+            error: error
+        })
+    }
+}
+
+export const loginRegister = async(req:Request, res: Response) =>{
+    try {
+        const {email, password} = req.body
+
+        const pool = await mssql.connect(sqlConfig)
+
+        let user = await (await pool.request().input("email", email).input("password", password).execute('loginUser')).recordset
+        
+        if(user[0]?.email  == email){
+            const CorrectPwd = await bcrypt.compare(password, user[0]?.password)
+
+            if(!CorrectPwd){
+                return res.status(401).json({
+                    message: "Incorrect password"
+                })
+            }
+
+            const LoginCredentials = user.map(records =>{
+                const {email,password, ...rest}=records
+
+                return rest
+            })
+
+            console.log(LoginCredentials);
+
+            // dotenv.config()
+            const token = jwt.sign(LoginCredentials[0], process.env.SECRET as string, {
+                expiresIn: '3600s'
+            })
+
+            return res.status(200).json({
+                message: "Logged in successfully", token
+            })
+            
+        }else{
+            return res.json({
+                message: "Email not found"
+            })
+        }
+
+    } catch (error) {
+        return res.json({
+            error: error
+        })
+    }
+}
+
+export const getAllUsers = async(req:Request, res:Response)=>{
+    try {
+
+        const pool = await mssql.connect(sqlConfig)
+
+        let users = (await pool.request().execute('fetchAllUsers')).recordset
+
+        return res.status(200).json({
+            users: users
+        })
+        
+    } catch (error) {
+        return res.json({
+            error: error
+        })
+    }
+}
+
+export const checkUserDetails = async (req:ExtendedUser, res:Response)=>{
+    
+    if(req.info){
+
+
+        return res.json({
+            info: req.info
+        })
+    }
+}
