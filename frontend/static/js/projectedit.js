0b713453a4305cff2914073eb983d0011cc7da53
annanodiyo <annanodiyo@gmail.com>
2023-11-02 16:10:17 +0300

    second commit

--- frontend/static/js/projectedit.js
+++ frontend/static/js/projectedit.js
@@ -1,11 +1,8 @@
-// const { log } = require("console");
-
-// const { log } = require("console");
-
 const hideProjectBody = document.querySelector("#remove");
 const hideableContent = document.querySelector("#hideable-content");
 const formproject = document.getElementById("project_edit");
 const saveChangesBtn = document.getElementById("saveChangesBtn");
+const projectStatus = document.querySelector('input[type="text"]').value;
 const cancelChangeBtn = document.getElementById("cancelEdit");
 
 cancelChangeBtn.addEventListener("click", (e) => {
@@ -17,19 +14,19 @@ saveChangesBtn.addEventListener("click", (e) => {
 
   const projectName = document.querySelector('input[type="text"]').value;
   const projectDescription = document.querySelector("textarea").value;
-  const projectStatus = document.querySelector('input[type="text"]').value;
+  // const projectStatus = document.querySelector('input[type="text"]').value;
 
   if (projectName.trim() === "" && projectDescription.trim() === "") {
-    console.log("You cannot save empty changes");
+    alert("You cannot save empty changes");
   } else {
     console.log("Project Name:", projectName);
     console.log("Project Description:", projectDescription);
     console.log("Project Status:", projectStatus);
   }
 
-  console.log("Project Name:", projectName);
-  console.log("Project Description:", projectDescription);
-  console.log("Project Status:", projectStatus);
+  // console.log("Project Name:", projectName);
+  // console.log("Project Description:", projectDescription);
+  // console.log("Project Status:", projectStatus);
 });
 
 hideProjectBody.addEventListener("click", (e) => {
